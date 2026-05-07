9f48e9ddace6bc6ed266a7f89bf50c6a1f203a4b
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-05-07 17:16:37 +0900

    feat: modified primitive.py

--- primitive.py
+++ primitive.py
@@ -11,3 +11,44 @@ print(f"the count: {count} va type: {count_type}")
 result1 = count.bit_count()  # method
 result2 = count.numerator  # state
 print(result1, result2)
+
+print("====== string =======")
+# SRTRINGxam primitiv variable xisoblanadi lekn buxam o'ziniing method va statelariga ega xisoblanadi
+# STRING METHODLARI: upper() lower() title() find() replace()
+
+
+course = "AI Python FullStack"
+result = type(course)
+print(f"Yangi result (1): {result}")
+result = course.title()
+print(f"Yangi result (2): {result}")
+
+# .           METHODNI tekshirish yo'li variable omi va . nuqta bosilsa methodlar chiqadi
+
+# UPPPER Mmethod string dagi xarflarni katta xarifga o'zgartirib beradi
+result = course.upper()
+print(f"Yangi result (3): {result}")
+
+# REPLACE almashtirishda ishlatiladi
+result = course.replace("FullStack", "MasterClass")
+print(f"replace: {result}")
+
+print("====== boolean =======")
+# function > input() tpe() bool() int() str()
+y = input('give your value for y:')
+print("y:", y)
+
+# booleanga boglash
+result = y.isnumeric()  # ISNUMERIC son kiritilsa true xarf kritilsa false qaytaradi
+print(f"isnumeric: {result}")
+
+#           TRUTHY VA FALSY VALUELAR
+# TRUTHY > TRUE 100 -100 "ilyosbek"
+# FALSY > FALSE 0 "" none
+# FALSY QIYMATLARI AGAR ORASIDA RU QIYMAT BOLSA TRUE QAYTARADI (100)>true qiymat
+test_falsy = "" or False or None or 0 or 100
+print("the FALSY:", bool(test_falsy))
+
+# FALSY > FALSE 0 "" none
+test_true = "MIT"
+print("the TRUE:", bool(test_true))

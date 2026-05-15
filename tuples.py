e36c705f98920a1ac1d27df0314c243fd478da0c
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-05-15 23:57:29 +0900

    feat: modified tuples.py

--- tuples.py
+++ tuples.py
@@ -1,6 +1,6 @@
 ''' TUPLE
 (1) TUPLE  o'zi nima: typle va list
-(2)
+(2) AGRUMENTLARNI yoyish (unpacking)
 (3)
 '''
 print("===== TUPLE  o'zi nima: typle va list =====")
@@ -27,3 +27,64 @@ tuple_obj = ("mit", 100, True, None)
 
 print(hayvonlar[0])
 # hayvonlar[0] = "ot"
+
+print("===== AGRUMENTLARNI yoyish =====")
+gruppa = ["MIT", "FLEXY", "DEVEX", "MG"]
+# yoyish uchun maxsus (qavus) qo'yiladi va ichiga yoziladi
+# birlashtirish uchun esa * harf misol(z) yozsaxam bo'ladi print qilinganda list orqalik terminalga chiqadi yani [shuni ichida]
+(x, y, z, a) = gruppa
+print(f"the x: {x} and y: {y}")
+
+# *argument (args) > tuple xisoblanadi
+
+
+def calculate(*args):
+    print("*args >", args)
+    total = 1
+    for x in args:
+        total *= x
+    print(f"the type(args) value: {type(args)}")
+    print(f"the total value: {total}")
+    return total
+
+
+# call
+calculate(1, 7, 2, 3)
+print("="*15)
+calculate(0, 2, 300)
+print("="*15)
+calculate(5, 7)
+
+
+print("-------------")
+# **kwargs > dictionary
+
+
+def introduce(**kwargs):
+    print(f"the type(**kwargs) value: {type(kwargs)}")
+    print(f"Hi I am {kwargs["name"]} and I am{kwargs["age"]} year old! ")
+
+
+# Call
+introduce(name="Jastin", age=25)
+introduce(name="Shawn", age=25, singler=True)
+print("-------------")
+
+
+def greeting(*args, **kwargs):
+    print("*args >", args)
+    print("**kwargs >", kwargs)
+
+
+# Call
+greeting("hi", True, 10, name="John", age=22)
+
+
+print("========= (3) ZIP  =======")  # Iterable object zip
+tuple1 = (1, 2, 3, 4)
+tuple2 = ('a', 'b', 'c')
+
+zipped = zip(tuple1, tuple2)
+print("zipped:", zipped)
+resilt = list(zipped)
+print(f"the result: {resilt}")

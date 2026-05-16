5d3f0fd20b68a8e12e74b86e65c99f041d148dca
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-05-16 22:16:42 +0900

    feat: learn python array

---
+++ array.set.py
@@ -0,0 +1,25 @@
+''' ARRAY & SET
+(1) array
+(2) set
+(3) specific operators with set
+'''
+
+# arrayni ishlatmoqchi bo'lsak array nomlik packcageni ichiga kirib array contructorni qabul qilish kerak
+
+from array import array
+# juda katta xajmdagi sonlar ketma ketligi bo'lsa arraydan foydalanadi kichik xajmda esa listdan
+# (i , f ) > intijer
+numbers = array("i", [1, 4, 5, 7, 8, 41])
+print("number(1)", numbers)
+
+# methodlar
+numbers.append(100)
+numbers.insert(0, 14)
+print("number(2)", numbers)
+
+numbers.remove(5)
+numbers.pop()
+print("number(3)", numbers)
+
+del numbers[0:2]
+print("number(4)", numbers)

b5c28a28fa2c597ed96973a0e88a686a29c88441
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-05-17 14:56:07 +0900

    feat: Core package turtle simple example

---
+++ debugging.py
@@ -0,0 +1,19 @@
+''' Packages & Debugging
+   (1) PYTHON PACKAGE & CORE PACKAGE
+   (2) PACKAGE MANAGER & EXTERNAL PACKAGE
+   (3) DEBUGGING
+'''
+import turtle
+print("===== PYTHON PACKAGE & CORE PACKAGE =====")
+''' Python Package/ Modulelari: Core, File, External  '''
+""" CORE PACKAGE > pyrhon bilan yonmayon yurodigon package xisoblanadi """
+''' CORE PACKAGE => LINKI > https://docs.python.org/3/library'''
+# import orqalik ulanadi
+
+# Core
+t = turtle.Turtle()
+t.shape("turtle")
+t.speed(1)
+t.circle(100)
+
+turtle.done()
